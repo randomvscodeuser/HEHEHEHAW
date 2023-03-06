0e5845fdaf84a8b385064b5341c861910245c015
randomvscodeuser <107854815+randomvscodeuser@users.noreply.github.com>
2023-03-06 19:20:42 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,30 +1,41 @@
-function preload(){
+noseX = 0;
+noseY = 0;
 
+
+function preload(){
+    moustache = loadImage("https://i.postimg.cc/3x3QzSGq/m.png");
 }
 
 function setup(){
-    canvas = createCanvas(648, 488);
-    canvas.position(110, 250);
+    canvas = createCanvas(300,300);
+    canvas.center();
     video = createCapture(VIDEO);
+    video.size(300, 300);
     video.hide();
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("poseNet is initialized :DD");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x - 17.5;
+        noseY = results[0].pose.nose.y + 2;
+        console.log("nose x = " + noseX);
+        console.log("nose y = " + noseY);
+    }
 }
 
 function draw(){
-    fill(255, 0, 0);
-    stroke(255, 0, 0);
-circle(50, 50, 80);
-circle(50, 430, 80);
-circle(590, 50, 80);
-circle(590, 430, 80);
-    fill(0, 128, 0);
-    stroke(0, 128, 0);
-rect(90, 420, 460, 20);
-rect(90, 40, 460, 20);
-rect(40, 90, 20, 300);
-rect(580, 90, 20, 300);
-image(video, 230, 150, 220, 200);
+    image(video, 0, 0, 300, 300);
+    image(moustache, noseX, noseY, 35, 35);
 }
 
 function take_snapshot(){
-    save('image.png')
+    save('moustache_lol.png');
 }
